ba45caaee69ee6f58d97c4a69d10b66f31be40d8
root <root@conci.imedia.in.ua>
2017-06-18 11:00:07 -0400

    branch repair

---
+++ commonsettings/dectar_admin_settings.php
@@ -0,0 +1,16 @@
+<?php 
+$config['admin_id']='1';
+$config['admin_name']='admin';
+$config['site_contact_mail']='andrey@conci.info';
+$config['email_title']='conci';		
+$config['logo_image']='logo.png';
+$config['favicon_image']= 'favicon.png';		
+$config['meta_title']='conci';
+$config['meta_keyword']='conci';
+$config['meta_description']='conci';		
+$config['email']='andrey@conci.info';
+$config['admin_type']='super';
+$config['is_verified']='Yes';
+$config['status']='Active'; 
+$config['base_url']='http://conci.imedia.in.ua';
+?>
---
+++ xmpp-master/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * Database and other configuration here
+ */
+
+define('database_name','jabberd'); // MySQL Database Name
+define('database_host','localhost'); // MySQL Host Name
+define('database_user','root');	// MySQL user Name
+define('database_password','');	// MySQL Password
+
+define('vhost','localhost');	// Xmpp Host/IP
+define('vhost_name','casp83');	// Xmpp Service Name
+define('vhost_admin_name','admin');	// Xmpp Admin User Name (without service name)
+define('vhost_admin_password','123456');	// Xmpp Admin Password
